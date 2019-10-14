a4c52e121f029419c773fee685a623a0dc5c29c7
QuangThai <nguyenquangthaib1@gmail.com>
2019-10-14 15:55:44 +0700

    C#

---
+++ AppG12019/frmQuaTrinhHocTapChiTiet.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace AppG12019
+{
+    partial class frmQuaTrinhHocTapChiTiet
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numTuNam = new System.Windows.Forms.NumericUpDown();
+            this.numDenNam = new System.Windows.Forms.NumericUpDown();
+            this.txtHocTai = new System.Windows.Forms.TextBox();
+            this.btnBoQua = new System.Windows.Forms.Button();
+            this.btnDongY = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numTuNam)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numDenNam)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Từ Năm";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Đến Năm";
+            this.label2.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Học Tại";
+            this.label3.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // numTuNam
+            // 
+            this.numTuNam.Location = new System.Drawing.Point(151, 31);
+            this.numTuNam.Maximum = new decimal(new int[] {
+            2050,
+            0,
+            0,
+            0});
+            this.numTuNam.Name = "numTuNam";
+            this.numTuNam.Size = new System.Drawing.Size(162, 20);
+            this.numTuNam.TabIndex = 2;
+            this.numTuNam.Value = new decimal(new int[] {
+            2000,
+            0,
+            0,
+            0});
+            this.numTuNam.ValueChanged += new System.EventHandler(this.NumericUpDown1_ValueChanged);
+            // 
+            // numDenNam
+            // 
+            this.numDenNam.Location = new System.Drawing.Point(151, 78);
+            this.numDenNam.Maximum = new decimal(new int[] {
+            2050,
+            0,
+            0,
+            0});
+            this.numDenNam.Name = "numDenNam";
+            this.numDenNam.Size = new System.Drawing.Size(162, 20);
+            this.numDenNam.TabIndex = 2;
+            this.numDenNam.Value = new decimal(new int[] {
+            2000,
+            0,
+            0,
+            0});
+            this.numDenNam.ValueChanged += new System.EventHandler(this.NumDenNam_ValueChanged);
+            // 
+            // txtHocTai
+            // 
+            this.txtHocTai.Location = new System.Drawing.Point(151, 142);
+            this.txtHocTai.Multiline = true;
+            this.txtHocTai.Name = "txtHocTai";
+            this.txtHocTai.Size = new System.Drawing.Size(347, 160);
+            this.txtHocTai.TabIndex = 3;
+            // 
+            // btnBoQua
+            // 
+            this.btnBoQua.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnBoQua.Image = global::AppG12019.Properties.Resources.back_arrow;
+            this.btnBoQua.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnBoQua.Location = new System.Drawing.Point(419, 325);
+            this.btnBoQua.Name = "btnBoQua";
+            this.btnBoQua.Size = new System.Drawing.Size(88, 43);
+            this.btnBoQua.TabIndex = 4;
+            this.btnBoQua.Text = "Bỏ qua";
+            this.btnBoQua.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnBoQua.UseVisualStyleBackColor = true;
+            // 
+            // btnDongY
+            // 
+            this.btnDongY.Image = global::AppG12019.Properties.Resources.refresh;
+            this.btnDongY.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDongY.Location = new System.Drawing.Point(309, 325);
+            this.btnDongY.Name = "btnDongY";
+            this.btnDongY.Size = new System.Drawing.Size(87, 43);
+            this.btnDongY.TabIndex = 4;
+            this.btnDongY.Text = "Đồng ý";
+            this.btnDongY.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnDongY.UseVisualStyleBackColor = true;
+            // 
+            // frmQuaTrinhHocTapChiTiet
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnBoQua;
+            this.ClientSize = new System.Drawing.Size(621, 393);
+            this.Controls.Add(this.btnBoQua);
+            this.Controls.Add(this.btnDongY);
+            this.Controls.Add(this.txtHocTai);
+            this.Controls.Add(this.numDenNam);
+            this.Controls.Add(this.numTuNam);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmQuaTrinhHocTapChiTiet";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Thêm/Chỉnh sửa quá trình học tập";
+            ((System.ComponentModel.ISupportInitialize)(this.numTuNam)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numDenNam)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numTuNam;
+        private System.Windows.Forms.NumericUpDown numDenNam;
+        private System.Windows.Forms.TextBox txtHocTai;
+        private System.Windows.Forms.Button btnDongY;
+        private System.Windows.Forms.Button btnBoQua;
+    }
+}
---
+++ AppG12019/frmQuaTrinhHocTapChiTiet.cs
@@ -0,0 +1,47 @@
+﻿using AppG12019.DAL.Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppG12019
+{
+    public partial class frmQuaTrinhHocTapChiTiet : Form
+    {
+        public frmQuaTrinhHocTapChiTiet(QuaTrinhHocTap  quaTrinhHocTap)
+        {
+            InitializeComponent();
+            if(quaTrinhHocTap == null)
+            {
+                this.Text = "Thêm mới quá trình học tập";
+            }
+            else
+            {
+                this.Text = "Chỉnh sửa quá trình học tập";
+                numTuNam.Value = quaTrinhHocTap.TuNam;
+                numDenNam.Value = quaTrinhHocTap.DenNam;
+                txtHocTai.Text = quaTrinhHocTap.HocTai;
+            }
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NumericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NumDenNam_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
